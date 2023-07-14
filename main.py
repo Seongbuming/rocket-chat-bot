3220789381bf1190de658093dee047050543afdc
Seongbum Seo <seo@seongbum.com>
2023-07-14 09:49:24 +0900

    Add scheduled workflow

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-import config
+import os
 from rocket_chat import RocketChat
 from commit_targets import CommitTargets
 
 def main():
-    chat_api = RocketChat(config.BASE_URL, config.USERNAME, config.PASSWORD)
-    commit_targets = CommitTargets(chat_api)
-    group_id = chat_api.get_group_id("opensource")
-    commit_targets_dict = commit_targets.check_last_week_commits(group_id)
-    commit_targets.send_commit_targets_message(group_id, commit_targets_dict)
+    rocket_chat = RocketChat(
+        base_url=os.getenv('ROCKET_CHAT_INSTANCE'),
+        username=os.getenv('USERNAME'),
+        password=os.getenv('PASSWORD')
+    )
+
+    commit_targets = CommitTargets(rocket_chat)
+
+    group_id = rocket_chat.get_group_id('opensource')
+    commit_targets.check_last_week_commits(group_id)
+    commit_targets.send_commit_targets_message(group_id)
 
 if __name__ == "__main__":
     main()
